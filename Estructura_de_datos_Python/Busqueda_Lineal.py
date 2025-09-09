2dc8b22e353c746d1bbe09f6aa48db9ab924241a
Josema24-code <josemanuelaguallogil@gmail.com>
2025-09-08 21:00:26 -0700

    le agregue comentarios

--- Estructura_de_datos_Python/Busqueda_Lineal.py
+++ Estructura_de_datos_Python/Busqueda_Lineal.py
@@ -2,11 +2,13 @@ numeros = [40,80,90,20,10]
 # Pedir un numero para buscar
 numero_Buscar = int(input("ingrese un numero a buscar: "))
 encontrado = False
-
+# Recorrer la lista para buscar el numero
 for i in range(len(numeros)):
     if numeros[i] == numero_Buscar:
         print("Número encontrado en la posición:", i)
         encontrado = True
         break
+    
+# Si no se encontró el número
 if not encontrado: 
     print("Número no encontrado en la lista.")
--- Estructura_de_datos_Python/recorrido_Columnas.py
+++ Estructura_de_datos_Python/recorrido_Columnas.py
@@ -3,7 +3,8 @@ Matriz_numeros = [
     [4,5,6],
     [7,8,9]
 ]
-
+# Recorrer las columnas
 for j in range(len(Matriz_numeros[1])):
+    # Recorrer las filas
     for i in range(len(Matriz_numeros[0])):
         print(Matriz_numeros[i][j])
